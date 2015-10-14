7f3d81d3658b09b6700a716d8fabae88f5bcfafe
Kenneth Berland <ken@grandrounds.com>
2015-10-14 16:51:51 -0700

    add agent stuff

---
+++ ZZ-agent.sh
@@ -0,0 +1,4 @@
+export AGENT_DEFAULT_FULL_NAME="Kenneth Berland"
+export AGENT_DEFAULT_USERNAME="ken@grandrounds.com"
+export AGENT_DEFAULT_PASSWORD=`gpg -d ~/asp.gpg`
+
--- agent.sh
+++
@@ -1,2 +0,0 @@
-export AGENT_DEFAULT_FULL_NAME="Kenneth Berland"
-export AGENT_DEFAULT_USERNAME="ken@grandroundshealth.com"
