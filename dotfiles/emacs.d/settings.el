a77fbc59087bb52d56ba8179b7a7affb059ee3ce
Kenneth Berland <ken@grandrounds.com>
2016-08-21 10:33:33 -0700

    add emacs

---
+++ dotfiles/emacs.d/settings.el
@@ -0,0 +1,14 @@
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
+(setq js-indent-level 2)
+(setq css-indent-offset 2)
+(setq coffee-tab-width 2)
+
+
+(require 'whitespace)
+(setq whitespace-line-column 120) ;; limit line length
+(setq whitespace-style '(face lines-tail))
+
+(add-hook 'prog-mode-hook 'whitespace-mode)
