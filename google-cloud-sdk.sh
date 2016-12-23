1481d7995b4561354708b92d6f5ee74fbe6d73a7
Kenneth Berland <ken@hero.com>
2016-12-23 19:06:49 +0000

    only activate GCP is resident

--- google-cloud-sdk.sh
+++ google-cloud-sdk.sh
@@ -1,2 +1,5 @@
-. $HOME/google-cloud-sdk/completion.bash.inc
-. $HOME/google-cloud-sdk/path.bash.inc
+if [ -f $HOME/google-cloud-sdk/ ]
+then
+    . $HOME/google-cloud-sdk/completion.bash.inc
+    . $HOME/google-cloud-sdk/path.bash.inc
+fi
