d8c34b23ebfe6b5184fcba5528b4171efffc03ef
Kenneth Berland <ken@postmates.com>
2017-01-11 18:43:28 -0800

    add pyenv and virtualenv

--- aws-completion.sh
+++ aws-completion.sh
@@ -1,1 +1,7 @@
-complete -C '/usr/bin/aws_completer' aws
+COMPLETER_FILE_NAME=aws_completer
+
+COMPLETER=$(which $COMPLETER_FILE_NAME)
+
+if [ -x "$COMPLETER" ]; then
+    complete -C $COMPLETER aws
+fi
---
+++ pyenv-virutalenv.sh
@@ -0,0 +1,16 @@
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+# create a virtualenv with pyenv
+# $ pyenv virtualenv 2.7.10 my-virtual-env
+
+# list existing virtualenvs
+# $ pyenv virtualenvs
+
+# use a virtualenv
+# $ pyenv activate my-virtual-env
+# $ pyenv deactivate my-virtual-env
+
+# delete a virtualenv
+# $ pyenv uninstasll my-virtual-env
+
