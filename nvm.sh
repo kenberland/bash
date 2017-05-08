9b4edf63d714b58b13cd4acb67c689c37160c10d
Kenneth Berland <berlandk@amazon.com>
2017-05-08 15:49:57 -0700

    only nvm if nvm

--- nvm.sh
+++ nvm.sh
@@ -1,2 +1,5 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+if [ -x "$NVM_DIR" ]; then
+    [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+fi
