7d65c01b2a725592fd16d3599db7e0b7b62f1eeb
Kenneth Berland <ken@hero.com>
2020-03-14 10:07:19 -0700

    foo

--- docking_perms.sh
+++ docking_perms.sh
@@ -1,6 +1,6 @@
 # allow root to mess with your xsession
 
-if [ -n "$DISPLAY" ] && DISPLAY=":0"
-then
-    xhost +si:localuser:root > /dev/null
-fi
+#if [ -n "$DISPLAY" ] && DISPLAY=":0"
+#then
+#    xhost +si:localuser:root > /dev/null
+#fi
--- pyenv-virutalenv.sh
+++ pyenv-virutalenv.sh
@@ -1,8 +1,9 @@
-export PYENV_ROOT=$HOME/.pyenv
+export PYENV_ROOT="$HOME/.pyenv"
+
 if [ -x "$PYENV_ROOT" ]; then
     export PATH=$PYENV_ROOT/bin:$PATH
     eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
+#    eval "$(pyenv virtualenv-init -)"
 fi
 
 
