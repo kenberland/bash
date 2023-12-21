09111f510424f9f7c9c6d85f1da48786d4306aa8
Ken Berland <ken@modelcode.ai>
2023-12-21 09:49:22 -0800

    foo

--- intellij.sh
+++ intellij.sh
@@ -1,1 +1,2 @@
-export PATH=${HOME}/idea-IC-211.7628.21/bin:"${PATH}"
+export PATH=${HOME}/idea/bin:"${PATH}"
+#export GOOGLE_APPLICATION_CREDENTIALS=$HOME/.config/gcloud/application_default_credentials.json
