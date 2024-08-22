bc8f25c276e384c4e4e25c582e4f32d787c12712
Ken Berland <ken@modelcode.ai>
2024-08-22 16:48:11 -0700

    foo

---
+++ gcloud-docker.sh
@@ -0,0 +1,1 @@
+alias docker-auth='gcloud auth print-access-token | docker login -u oauth2accesstoken --password-stdin https://us-west1-docker.pkg.dev'
--- python.sh
+++ python.sh
@@ -1,1 +1,12 @@
 export PATH=$HOME/python-3.12/bin:$PATH
+
+function cd() {
+    builtin cd "$@"
+    if [ -d ".venv" ]; then
+        if [ -f ".venv/bin/activate" ]; then
+            source .venv/bin/activate
+            echo "Activated virtual environment in $(pwd)"
+        fi
+    fi
+}
+
